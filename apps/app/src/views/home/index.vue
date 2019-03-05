5e03ba08e071189585f324b2124c53fe410e40f3
付建邦 <fujianbang0ops@gmail.com>
2019-03-06 00:46:58 +0800

    add home page and jpg

--- apps/app/src/views/home/index.vue
+++ apps/app/src/views/home/index.vue
@@ -1,5 +1,49 @@
 <template>
-    <div>
-        首页
+    <div class="welcome">
+        <div class="left">
+            <div class="logo-area">
+                <img :src="TracyImg" />
+            </div>
+            <div class="info-area">
+                <h2>陈镜璇的Word检查工具</h2>
+                <p>{{'Ver.' + version}}</p>
+            </div>
+            <p class="time">
+            last update:
+            <Time :time="lastUpdateDate"></Time>
+            </p>
+        </div>
+        <div class="right">
+            <div class="logo-area">
+                <img :src="StefanieImg" />
+            </div>
+            <div class="button-area">
+                <Button type="error" ghost @click="enterHandle">立即进入</Button>
+            </div>
+        </div>
     </div>
 </template>
+<script>
+import TracyImg from '@/assets/tracy.jpg'
+import StefanieImg from '@/assets/stefanie.jpg'
+export default {
+    data() {
+        return {
+            TracyImg,
+            StefanieImg,
+            version: '20190306',
+            lastUpdateDate: new Date()
+        }
+    },
+    methods: {
+        enterHandle() {
+            // TODO
+        }
+    },
+    mounted() {
+    }
+}
+</script>
+<style lang="less" scoped>
+@import './style.less';
+</style>
