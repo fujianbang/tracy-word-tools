3ea884ecf5738d323f876d385cf0ce6a3d1f1cb1
付建邦 <fujianbang0ops@gmail.com>
2019-03-22 16:32:08 +0800

    add antd and update landingPage.vue

--- apps/ele-app/src/renderer/components/LandingPage.vue
+++ apps/ele-app/src/renderer/components/LandingPage.vue
@@ -1,128 +1,104 @@
 <template>
-  <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
-    <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
-        <system-information></system-information>
+  <div class="welcome">
+    <div class="left">
+      <div class="logo-area">
+        <img :src="TracyImg">
       </div>
-
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
+      <div class="info-area">
+        <h2>陈镜璇的Word检查工具</h2>
+        <p>{{'Ver.' + version}}</p>
       </div>
-    </main>
+      <p class="time">
+        last update:
+        <Time :time="lastUpdateDate"></Time>
+      </p>
+    </div>
+    <div class="right">
+      <div class="logo-area">
+        <img :src="StefanieImg">
+      </div>
+      <div class="button-area">
+        <a-button type="primary" ghost @click="enterHandle">立即进入</a-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import SystemInformation from './LandingPage/SystemInformation'
-
-  export default {
-    name: 'landing-page',
-    components: { SystemInformation },
-    methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
-      }
+import TracyImg from "@/assets/tracy.jpg";
+import StefanieImg from "@/assets/stefanie.jpg";
+export default {
+  data() {
+    return {
+      TracyImg,
+      StefanieImg,
+      version: "20190306",
+      lastUpdateDate: new Date()
+    };
+  },
+  methods: {
+    /*
+     * 进入处理页面
+     */
+    enterHandle() {
+      this.$router.push({
+        name: "handle"
+      });
     }
-  }
+  },
+  mounted() {}
+};
 </script>
 
-<style>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
-
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-
-  body { font-family: 'Source Sans Pro', sans-serif; }
-
-  #wrapper {
-    background:
-      radial-gradient(
-        ellipse at top left,
-        rgba(255, 255, 255, 1) 40%,
-        rgba(229, 229, 229, .9) 100%
-      );
-    height: 100vh;
-    padding: 60px 80px;
-    width: 100vw;
-  }
-
-  #logo {
-    height: auto;
-    margin-bottom: 20px;
-    width: 420px;
-  }
-
-  main {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  main > div { flex-basis: 50%; }
-
-  .left-side {
-    display: flex;
-    flex-direction: column;
-  }
-
+<style lang="less" scoped>
   .welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
-  }
-
-  .title {
-    color: #2c3e50;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
-
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-
-  .doc p {
-    color: black;
-    margin-bottom: 10px;
-  }
-
-  .doc button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
-  }
-
-  .doc button.alt {
-    color: #42b983;
-    background-color: transparent;
-  }
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    .left {
+        flex: 1 1 auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        .logo-area img {
+            width: 300px;
+            height: 300px;
+            display: inherit;
+            margin: 0 auto;
+        }
+        .info-area {
+            h2 {
+                text-align: center;
+            }
+            p {
+                text-align: center;
+            }
+        }
+        p.time {
+            text-align: center;
+            margin-top: 20px;
+        }
+    }
+    .right {
+        flex: 0 0 400px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
+        .logo-area img {
+            width: 200px;
+            height: 200px;
+            display: inherit;
+            margin: 0 auto;
+            border-radius: 20px;
+        }
+        .button-area {
+            button {
+                width: 200px;
+                margin: 0 auto;
+                display: inherit;
+            }
+        }
+    }
+}
 </style>
